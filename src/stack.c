3453aa73a32cc04674fdaba89fc695fc734f3f2e
TheVirginCoder <nfisher2670@gmail.com>
2018-12-24 20:30:52 +0000

    Added push function

--- src/stack.c
+++ src/stack.c
@@ -1,17 +1,44 @@
+/* 
+ *
+ * This program is a basic implementation of the Stack data structure in the
+ * C programming language. It includes pop and push functionality
+ * and uses a typedef for the struct that stores the top of the stack
+ * 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_STACK_SIZE 1024
+#define MAX_STACK_SIZE 1024 /* Maximum size of the stack */
 
+/* The stack data structure, implemented as a typedef */
 typedef struct {
         int data[MAX_STACK_SIZE];
         int top;
 } stack;
 
-stack init_stack(void)
+/* This is a function to initialise the stack structure, returns a pointer */
+stack *init_stack(void)
 {
+        /* Dynamically allocate size of the stack structure */
         stack *new_stack = (stack *)malloc(sizeof(stack));
-        new_stack->top = -1;
+        new_stack->top = -1; /* Set top to -1 (Empty stack) */
+
+        return new_stack;
+}
+
+/* Function to push (add) values to stack */
+void push(stack *modstack, int val)
+{
+        /* Check if the stack is full */
+        if (modstack->top < MAX_STACK_SIZE) {
+                /* Increment top and add value */
+                modstack->top++;
+                modstack->data[modstack->top] = val;
+        } else {
+                /* Print error to stderr if stack is full */
+                fprintf(stderr, "Err: Stack full.\n");
+        }
 }
 
 int main(void)
